bfc13d09d43bf57e112f94af117addb06eca09f5
Frank <fjcarnevale@gmail.com>
2015-02-06 21:57:30 -0500

    Initial commit, basic models

---
+++ matchday.py
@@ -0,0 +1,76 @@
+import webapp2
+import jinja2
+import os
+
+from webapp2_extras import sessions
+
+from models import League
+
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'my-super-secret-key',
+}
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+# Sourced from https://webapp-improved.appspot.com/api/webapp2_extras/sessions.html
+class BaseHandler(webapp2.RequestHandler):
+  def dispatch(self):
+    # Get a session store for this request.
+    self.session_store = sessions.get_store(request=self.request)
+
+    try:
+      # Dispatch the request.
+      webapp2.RequestHandler.dispatch(self)
+    finally:
+      # Save all sessions.
+      self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+      # Returns a session using the default cookie key.
+      return self.session_store.get_session()
+
+class Index(BaseHandler):
+  """ Handles requests to the main page """
+  def get(self):
+
+    template = JINJA_ENVIRONMENT.get_template('index.html')
+    self.response.write(template.render())
+
+class CreateLeague(BaseHandler):
+  def get(self):
+    league = League()
+    league.name = self.request.get("name")
+    league.put()
+
+    self.response.write("Created league %s" % league.name)
+
+    leagues = League.get_leagues()
+
+    for l in leagues:
+      print l
+
+# Path mappings
+application = webapp2.WSGIApplication([
+  ('/', CreateLeague),
+], config=config, debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ models.py
@@ -0,0 +1,37 @@
+from google.appengine.ext import ndb
+
+class League(ndb.Model):
+  name = ndb.StringProperty()
+  teams = ndb.KeyProperty(repeated=True)
+  matches = ndb.KeyProperty(repeated=True)
+
+  def add_new_team(self, name):
+    team = Team.new_team(name,self)
+    team_key = team.put()
+
+    self.teams.append(team_key)
+    self.put()
+    return team_key
+
+  @classmethod
+  def get_leagues(cls):
+    return cls.query()
+
+class Team(ndb.Model):
+  name = ndb.StringProperty()
+  league = ndb.KeyProperty()
+  matches = ndb.KeyProperty(repeated=True)
+
+  @staticmethod
+  def new_team(name, league):
+    team = Team()
+    team.populate(name = name, league = league.key)
+    return team
+
+class Match(ndb.Model):
+  date = ndb.DateTimeProperty()
+  teams = ndb.KeyProperty(repeated=True)
+  league = ndb.KeyProperty()
+
+
+
